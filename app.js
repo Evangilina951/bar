0f56d558cef20858a3b72c7c6b1d5e19315a5d3b
Evangilina951 <0457376@mail.ru>
2025-07-18 20:11:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -471,7 +471,7 @@ function initializeApp() {
         const catData = cat.data();
         list.innerHTML += `
           <li class="flex items-center justify-between p-2 border-b">
-            <span>${catData.name} (Порядок: ${catData YOURLS_0x0D catData.number}, Видимость: ${catData.isVisible ? 'Вкл' : 'Выкл'})</span>
+            <span>${catData.name} (Порядок: ${catData.number}, Видимость: ${catData.isVisible ? 'Вкл' : 'Выкл'})</span>
             <div class="flex gap-2">
               <button onclick="loadCategoryForEdit('${cat.id}')" class="bg-yellow-600 text-white p-1 rounded">Редактировать</button>
               <button onclick="deleteCategory('${cat.id}')" class="bg-red-600 text-white p-1 rounded">Удалить</button>
@@ -833,7 +833,7 @@ function initializeApp() {
           <td class="border border-gray-300 p-2 text-center">
             <button onclick="loadIngredientForEdit('${ing.id}')" class="bg-yellow-600 p-2 rounded">
               <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m- любых1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
               </svg>
             </button>
             <button onclick="deleteIngredient('${ing.id}')" class="bg-red-600 p-2 rounded ml-2">
